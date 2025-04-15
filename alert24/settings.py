008aa0529fa199ad49759a93db7caf70dfe0c790
PranjaliDwivedi <pranjalidwivedi.apn@gmail.com>
2025-04-16 00:00:59 +0530

    Update settings.py

--- alert24/settings.py
+++ alert24/settings.py
@@ -22,8 +22,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main'
-    'whitenoise'
+    'main',
+    'whitenoise',
 ]
 
 MIDDLEWARE = [
